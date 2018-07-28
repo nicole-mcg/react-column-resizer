4acb06c285c695503173db67f644316f6ccc3ed6
Connor <c.mcg132@gmail.com>
2018-07-28 11:28:19 -0400

    Fixed import error

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 'use strict';
-module.exports = function () {
-  throw new Error("Don't instantiate directly! Use require('react-column-resizer')");
-};
-
-module.exports.Rows = require('./build/dist.js').ColumnResizer;
+module.exports = require('./build/dist.js');
